01cbd8ca6d2db1b4186e795cb0cbddaf9f4f9bcc
ducs1405 <ducs1405@gmail.com>
2022-07-28 19:20:36 +0700

    test

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import classNames from "classnames/bind";
+import styles from './Header/module.scss';
+
+const cx = classNames.bind(styles)
+
+function Header() {
+    return(
+        <h2></h2>
+    )
+}
+
+export default Header;
